52ca7976f62cf48059905c4b256334dc4eb7fc83
Sathivilli Deekshitha <deekshisathivilli@gmail.com>
2026-07-23 13:54:27 +0530

    Delete week 5 directory

--- week 5/12_ReactJS-HOL/Code/ticketbookingapp/src/GuestGreeting.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function GuestGreeting() {
-  return <h2 className="greeting-text">Please sign up to book tickets or browse flight details.</h2>;
-}
-
-export default GuestGreeting;
